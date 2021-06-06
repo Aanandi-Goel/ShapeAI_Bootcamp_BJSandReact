7b44e38121f299192ded34e1454e271729f38cd0
Aanandi-Goel <70386565+Aanandi-Goel@users.noreply.github.com>
2021-06-06 16:25:48 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  var cy = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {cy}</p>
+    </footer>
+  );
+}
+export default Footer;
